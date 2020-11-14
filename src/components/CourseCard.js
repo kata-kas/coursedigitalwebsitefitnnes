99c9770b06fcf64274d01bfb070444331dba222a
catalin <catalin.casuneanu@gmail.com>
2020-11-14 12:44:36 +0200

    update

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,19 +1,21 @@
 import '../App.css';
 import {Card, Button} from 'react-bootstrap';
-import coursecardimg from '../assets/coursecard.jpg';
+import coursecardimg from '../assets/cardcourse.png';
 function CourseCard () {
     return (
         <div className="full">
         <Card border="warning" style={{ width: 'auto', marginLeft:'20%', marginRight:'20%' }}>
-        <Card.Img variant="top" src={coursecardimg} />
+        <Card.Img variant="top" src={coursecardimg} alt="Curs Fittnes pentru începători" />
         <Card.Body>
         <Card.Title style={{textAlign: 'center'}}> Curs "Fitness pentru începători"</Card.Title>
         <Card.Text style={{textAlign: 'center'}}>
                     <p><strong>Data:</strong> 20.11.2020</p><br/>
-                    <p><strong>Cost:</strong> 100 RON</p><br/><br/>
+                    <p><strong>Cost:</strong> 200 RON</p><br/><br/>
         </Card.Text>
-        <Button className="mx-auto" variant="warning">Înscrie-te</Button>
-        </Card.Body>
+        <div style={{textAlign:'center'}}>
+        <Button variant="warning">Înscrie-te</Button>
+        </div> 
+       </Card.Body>
         </Card>
        </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import logo from '../assets/logo.svg';
+import {Link} from 'react-router-dom';
+
+
 function Header() {
     return(
         <div className="App">
         <header className="App-header">
+        <Link to="/" >
         <img src={logo} className="App-logo" alt="NOSS logo" />
+        </Link>
         </header>
         </div>
     );
