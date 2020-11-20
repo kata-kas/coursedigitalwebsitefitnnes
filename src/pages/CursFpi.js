12cc11e8327b54558c8abdae6d6d63d210eeba01
catalin <catalin.casuneanu@gmail.com>
2020-11-20 13:19:52 +0200

    course structure

--- src/pages/CursFpi.js
+++ src/pages/CursFpi.js
@@ -76,6 +76,11 @@ function CursFpi(){
         <div style={{margin:'auto'}}>
         <ReactPlayer url='https://www.youtube.com/watch?v=NdaTY4ZoIzI' loop="true" playing="true" 
         width="100%" height="400px"/></div>
+        <h2>Care este structura cursului? 🗺️</h2>
+        <p>Cursul va avea loc în grupul nostru oficial, pe aplicația Telegram. Astfel vei face parte din comunitatea NOSS, iar evoluția ta către un stil de viață mai sănătos va fi mai ușoară </p>
+        <p>Cursul este împărțit pe o perioada de 4 săptămâni, în lecții separate, exercițiile propuse la fiecare lecție vor fi în format video, executate de către antrenorul oficial NOSS, Laurențiu Spătaru.</p>
+        <p>La finalul acestui curs vei primi o diplomă de absolvire și vei face parte din comunitatea NOSS Premium, care are acces la noile noastre cursuri înainte celorlalți.</p>
+        <p>Că să începi această călătorie alături de noi, apasă butonul de mai jos!</p>
         </Card.Text>
         <Card.Body>
         <div >
