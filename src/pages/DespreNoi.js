1f13c59cd7629872e0f59b65eeb7d8facb78adab
catalin <catalin.casuneanu@gmail.com>
2021-01-06 21:45:49 +0200

    errors

--- src/pages/DespreNoi.js
+++ src/pages/DespreNoi.js
@@ -1,6 +1,5 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import {Link} from 'react-router-dom';
 import Typed from 'react-typed';
 
 function DespreNoi(){
