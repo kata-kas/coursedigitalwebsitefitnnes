d58ebdc9712b357b52fadbed3d79bf9d014f71e7
catalin <catalin.casuneanu@gmail.com>
2021-01-22 16:25:28 +0200

    videos

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,52 +1,90 @@
-import '../App.css';
-import {Card} from 'react-bootstrap';
-import React from 'react';
-import {Link} from 'react-router-dom';
+import "../App.css";
+import { Card } from "react-bootstrap";
+import React from "react";
+import { Link } from "react-router-dom";
 
-function CourseCard () {
-    return (
-        <div className="full" >
-        <Card style={{ width: 'auto', backgroundColor:'rgb(29, 29, 29)', color:' #CA2000' }}>
-        <Card.Body>
-        <video preload="none" controls poster="https://storage.googleapis.com/kreativio/banner2.png" id="bun-venit">
-      <source src='https://storage.googleapis.com/kreativio/Instructiuni.mp4' type='video/mp4' />
-    </video>
-        <Card.Title style={{textAlign: 'left', fontFamily:'Raleway',
-        margin:'10%'}}><strong> Curs "Fitness pentru un stil de viață sănătos"</strong></Card.Title>
-        <Card.Text style={{textAlign: 'left', lineHeight:'20px', margin:'10%', fontWeight:'bold'}}>
-                     18 Ianuarie 2021<br/>
-                     30 zile<br/>
-                    200 RON<br/><br/>
-                    <Link to="/curs-fitness-pentru-incepatori"
-                    style={{textDecoration: 'none', color: ' #CA2000', fontFamily:'Raleway', fontSize:'20px',
-                  }}
-                    >Detalii</Link>
-        </Card.Text>
-        <div >
-        <Link to="/inscriere">
-        <div style={{
-          bottom: '2%',
-          left:'50%',  transform: 'translate( -50%, 0%)',
-          width: '150px',
-          height: '50px',
-          fontSize: '18px',
-          fontWeight: '900',
-          textTransform: 'uppercase',
-          textAlign: 'center',
-          backgroundColor: '#CA2000',
-          color: 'white',
-          padding: '1%',
-          borderRadius: '1%',
-          position: 'absolute',
+function CourseCard() {
+  return (
+    <div className="full">
+      <Card
+        style={{
+          width: "auto",
+          backgroundColor: "rgb(29, 29, 29)",
+          color: " #CA2000",
         }}
-      
-       >Înscrie-te</div>
-       </Link>
-        </div> 
-       </Card.Body>
-        </Card>
-       </div>
-    );
+      >
+        <Card.Body>
+          <video
+            preload="none"
+            controls
+            poster="https://evc59.pcloud.com/dpZ7Bq0C7ZAUeeskZUpv7ZZ4SkuG7Z3VZZfs0ZXZtE73tCnQawytSWn436IjFhTC3Pyy/th-2084030952-955x708.png"
+            id="bun-venit"
+          >
+            <source
+              src="https://storage.googleapis.com/kreativio/Instructiuni.mp4"
+              type="video/mp4"
+            />
+          </video>
+          <Card.Title
+            style={{ textAlign: "left", fontFamily: "Raleway", margin: "10%" }}
+          >
+            <strong> Curs "Fitness pentru un stil de viață sănătos"</strong>
+          </Card.Title>
+          <Card.Text
+            style={{
+              textAlign: "left",
+              lineHeight: "20px",
+              margin: "10%",
+              fontWeight: "bold",
+            }}
+          >
+            18 Ianuarie 2021
+            <br />
+            30 zile
+            <br />
+            200 RON
+            <br />
+            <br />
+            <Link
+              to="/curs-fitness-pentru-incepatori"
+              style={{
+                textDecoration: "none",
+                color: " #CA2000",
+                fontFamily: "Raleway",
+                fontSize: "20px",
+              }}
+            >
+              Detalii
+            </Link>
+          </Card.Text>
+          <div>
+            <Link to="/inscriere">
+              <div
+                style={{
+                  bottom: "2%",
+                  left: "50%",
+                  transform: "translate( -50%, 0%)",
+                  width: "150px",
+                  height: "50px",
+                  fontSize: "18px",
+                  fontWeight: "900",
+                  textTransform: "uppercase",
+                  textAlign: "center",
+                  backgroundColor: "#CA2000",
+                  color: "white",
+                  padding: "1%",
+                  borderRadius: "1%",
+                  position: "absolute",
+                }}
+              >
+                Înscrie-te
+              </div>
+            </Link>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default CourseCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
-import '../App.css';
-import Benefits from '../components/Benefits';
-import Header from '../components/Header';
-import CourseCard from '../components/CourseCard';
-import Footer from '../components/Footer';
-import {Link} from 'react-router-dom';
-import { motion } from 'framer-motion'
+import "../App.css";
+import Benefits from "../components/Benefits";
+import Header from "../components/Header";
+import CourseCard from "../components/CourseCard";
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import React, { useState, useEffect } from "react";
 function Home() {
   const [allowVideo, setAllowVideo] = useState(false);
@@ -19,64 +19,94 @@ function Home() {
   const variants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 },
-  }
+  };
   return (
-    <motion.div >
-
-     <div className="App">
-       <div>
-     {allowVideo && (
-       <video
-       muted
-       loop
-       autoPlay
-       playsInline
-       onCanPlayThrough={videoLoaded}
-       poster="https://storage.googleapis.com/kreativio/banner.jpg"
-       className={`${isVideoLoaded ? "show" : "hide"}` }
-       id="background-video"
-     >
-      <source src='https://storage.googleapis.com/kreativio/video.mp4' type='video/mp4' />
-    </video>
-     )}</div>
-        <div className="Container" style={{height:'100vh'}}>
-        <Header/>
-          <motion.div initial="hidden"
-    animate="visible"
-    variants={variants}
-    transition={{duration:5}}
-    style={{position:'absolute', top:'50%', left:'50%',  transform: 'translate( -50%, -50%)', fontSize: '70px', fontWeight: '800', lineHeight: '1.3em', color:'#CA2000', width:'70%'
-  , padding:'0.5%', textAlign:'center'}}
-    >
-          NOSS te ajută să fii în formă maximă! 
-          <br/>
-          <br/>
-          </motion.div> 
+    <motion.div>
+      <div className="App">
+        <div>
+          {allowVideo && (
+            <video
+              muted
+              loop
+              autoPlay
+              playsInline
+              onCanPlayThrough={videoLoaded}
+              poster="https://storage.googleapis.com/kreativio/banner.jpg"
+              className={`${isVideoLoaded ? "show" : "hide"}`}
+              id="background-video"
+            >
+              <source
+                src="https://edef3.pcloud.com/cBZ7pe0C7ZKBJeskZZZXQkuG7ZQ5ZZfs0ZkZHQABZSZMZ57ZUZmZwZR7Zy7ZH7Zm7ZvZlZrZWZLGo7Z70aEjzEWhijtN3JHzEURXFiT8SKk/Hero.mp4"
+                type="video/mp4"
+              />
+            </video>
+          )}
+        </div>
+        <div className="Container" style={{ height: "100vh" }}>
+          <Header />
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+            transition={{ duration: 5 }}
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate( -50%, -50%)",
+              fontSize: "70px",
+              fontWeight: "800",
+              lineHeight: "1.3em",
+              color: "#CA2000",
+              width: "70%",
+              padding: "0.5%",
+              textAlign: "center",
+            }}
+          >
+            NOSS te ajută să fii în formă maximă!
+            <br />
+            <br />
+          </motion.div>
           <div>
-          <Link to="/despre-noi" className="Btn" style={{textDecoration:'none'}}>
-          AFLĂ CUM!
-          </Link></div>
-          
+            <Link
+              to="/despre-noi"
+              className="Btn"
+              style={{ textDecoration: "none" }}
+            >
+              AFLĂ CUM!
+            </Link>
+          </div>
+        </div>
       </div>
-      </div>
-    <Benefits/>
-    <div className="videostore">
-    {allowVideo && (
+      <Benefits />
+      <div className="videostore">
+        {allowVideo && (
           <video
             controls
             autoPlay
             playsInline
             onCanPlayThrough={videoLoaded}
             className={`${isVideoLoaded ? "show" : "hide"}`}
-          poster="https://storage.googleapis.com/kreativio/banner.jpg" id="bun-venit">
-      <source src='https://storage.googleapis.com/kreativio/Bun%20venit.mp4' type='video/mp4' />
-    </video>)}
-    </div>
-    <CourseCard/>
-    <Footer/>
-    <script id="CookieDeclaration" src="https://consent.cookiebot.com/98f96dbb-884d-4981-bd1e-59204bc2e36a/cd.js" type="text/javascript" async></script>
+            poster="https://evc59.pcloud.com/dpZ7Bq0C7ZAUeeskZUpv7ZZ4SkuG7Z3VZZfs0ZXZtE73tCnQawytSWn436IjFhTC3Pyy/th-2084030952-955x708.png"
+            id="bun-venit"
+          >
+            <source
+              src="https://edef3.pcloud.com/cBZCVM0C7ZqLHeskZZZymkuG7ZQ5ZZfs0ZkZaaH5kZuZxZJ7ZNZy7ZvZL7ZiZhZp7Z2ZKZeZb7Z2Go7ZENyDc5kJg1plydLLixS6Nhi9bdNV/bv.mp4"
+              type="video/mp4"
+            />
+          </video>
+        )}
+      </div>
+      <CourseCard />
+      <Footer />
+      <script
+        id="CookieDeclaration"
+        src="https://consent.cookiebot.com/98f96dbb-884d-4981-bd1e-59204bc2e36a/cd.js"
+        type="text/javascript"
+        async
+      ></script>
     </motion.div>
-      );
+  );
 }
 
 export default Home;
